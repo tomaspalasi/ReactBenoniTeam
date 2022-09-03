02786b6c244c5480474f4dd91f02157ce81db614
TJP <tomi.palasi@gmail.com>
2022-09-03 13:49:44 -0300

    agregado SweetAlert

--- src/components/Body/Prendas/Button.jsx
+++ src/components/Body/Prendas/Button.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+
+function AgregadoAlCarrito (){
+    return (
+        Swal.fire ({
+            title: "¡Producto agregado al carrito!",
+            position: "top-end",
+            toast: true,
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            color: "whitesmoke",
+            background: "rgb(32, 32, 32)",
+        })
+    )
+}
 
 const SuperButton = () => {
     return (
-        <input type="button" value="Agregar al Carrito"/>
+        <input type="button" value="Agregar al Carrito" onClick={AgregadoAlCarrito}/>
     )
 }
 export default SuperButton;
