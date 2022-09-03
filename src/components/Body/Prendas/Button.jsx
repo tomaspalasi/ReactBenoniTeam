9ba26fa1871dcfc2482e408d15b70f10ee56c339
TJP <tomi.palasi@gmail.com>
2022-09-03 16:39:24 -0300

    agregado contador de productos

--- src/components/Body/Prendas/Button.jsx
+++ src/components/Body/Prendas/Button.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import Swal from 'sweetalert2/dist/sweetalert2.js'
 
 function AgregadoAlCarrito (){
@@ -17,8 +17,24 @@ function AgregadoAlCarrito (){
 }
 
 const SuperButton = () => {
+
+    let stock= 20;
+    let [cantidad, setCantidad] = useState(0);;
+    let cantidadRemeras = cantidad;
+    
     return (
-        <input type="button" value="Agregar al Carrito" onClick={AgregadoAlCarrito}/>
+        <div>
+            <input type="button" value="+" onClick={() => {
+                if (cantidad !== stock) {setCantidad (cantidad + 1)}
+            }}/>
+            <h4>{cantidadRemeras}</h4>
+            <input type="button" value="-" onClick={() => {
+                if (cantidad === 0) {cantidad = 0} else {setCantidad (cantidad - 1)}
+            }}/>
+            <div>
+            <input type="button" value="Agregar al Carrito" onClick={AgregadoAlCarrito}/>
+            </div>
+        </div>
     )
 }
 export default SuperButton;
--- src/components/Body/Productos.jsx
+++ src/components/Body/Productos.jsx
@@ -9,30 +9,35 @@ const Productos = () => {
             nombre: "BENONI",
             precio: 2500,
             img: "https://benoniteamgym.netlify.app/assets/img/remeras/remera_1-logo_benoni-dorso.png",
+            stock: 25,
         },
         {
             id: 2,
             nombre: "BIELAS",
             precio: 2400,
             img: "https://benoniteamgym.netlify.app/assets/img/remeras/remera_2-bielas-dorso.png",
+            stock: 20,
         },
         {
             id: 3,
             nombre: "DAVAI",
             precio: 2800,
             img: "https://benoniteamgym.netlify.app/assets/img/remeras/remera_3-davai-dorso.png",
+            stock: 12,
         },
         {
             id: 4,
             nombre: "DAVAI!!",
             precio: 3000,
             img: "https://benoniteamgym.netlify.app/assets/img/remeras/remera_4-davai_2-dorso.png",
+            stock: 14,
         },
         {
             id: 5,
             nombre: "MERENGUITOS",
             precio: 4000,
             img: "https://benoniteamgym.netlify.app/assets/img/remeras/remera_5-merenguitos-dorso.png",
+            stock: 10,
         }
     ]
     )
@@ -46,6 +51,7 @@ const Productos = () => {
                     img={prenda.img}
                     nombre={prenda.nombre}
                     precio={prenda.precio}
+                    stock={prenda.stock}
                     />
             )
         })}
